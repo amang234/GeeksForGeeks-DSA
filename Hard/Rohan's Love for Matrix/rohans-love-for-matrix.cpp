54cd864f53b92725bb63f739e7182a3e2293ad76
Aman Grover <60424294+amang234@users.noreply.github.com>
2024-04-23 09:49:16 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int firstElement(int n) {
+        int a=0,b=1;
+        int mod=1e9+7;
+        while(n--){
+            int c=(a+b)%mod;
+            a=b%mod;
+            b=c%mod;
+        }
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.firstElement(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
