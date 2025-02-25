dfa25d6fb4f488da6cfeceb2402ee5d0d896776d
Aman Grover <60424294+amang234@users.noreply.github.com>
2025-02-25 09:59:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMaxArea(vector<int> &arr) {
+        // Your code here
+        int n=arr.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        stack<int> s;
+        
+        //next smaller element---
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]>arr[i]){
+                right[s.top()]=i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        while(!s.empty()){
+            right[s.top()]=n;
+            s.pop();
+        }
+        
+        // stack should be empty---
+        while(!s.empty()){
+            s.pop();
+        }
+        
+        //previous smaller element---
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && arr[s.top()]>arr[i]){
+                left[s.top()]=i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        
+        while(!s.empty()){
+            left[s.top()]=-1;
+            s.pop();
+        }
+        
+        //calculate the max-area
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,arr[i]*(right[i]-left[i]-1));
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.getMaxArea(arr);
+        cout << res << endl;
+        cout << "~" << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
