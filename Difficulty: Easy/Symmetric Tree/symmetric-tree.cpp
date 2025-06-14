35c14cd298cee85acd4076f26cd767957abdb6b1
Aman Grover <60424294+amang234@users.noreply.github.com>
2025-06-14 21:52:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    bool solve(Node* r1, Node* r2){
+        if(r1==NULL && r2==NULL) return 1;
+        if((r1==NULL && r2!=NULL) || (r1!=NULL && r2==NULL)) return 0;
+        return ((r1->data==r2->data) && solve(r1->left, r2->right) 
+        && solve(r1->right, r2->left));
+    }
+    
+    // return true/false denoting whether the tree is Symmetric or not
+    bool isSymmetric(struct Node* root)
+    {
+	    // Code here
+	    if(root==NULL) return 1;
+	    return solve(root->left, root->right);
+        
+    }
+};
