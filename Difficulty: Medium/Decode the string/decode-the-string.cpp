75bd0c5a8fd0ad471eedcf3f5850fa119f78b8db
Aman Grover <60424294+amang234@users.noreply.github.com>
2025-09-17 13:55:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.cpp
+++ Difficulty: Medium/Decode the string/decode-the-string.cpp
@@ -1,82 +1,38 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
-    string decodedString(string s) {
-        int n = s.length();
-        return parse(s, 0).first; // Start parsing from index 0
-    }
-
-private:
-    pair<string, int> parse(string& s, int i) {
-        string r = "";
-        int n = s.length();
-        while (i < n) {
-            auto result = parseItem(s, i);
-            string p = result.first;
-            i = result.second;
-            if (p == string(1, '-1')) { // Stopper value
-                break;
+    string decodedString(string &s) {
+        // code here
+        stack<string> st;
+        int len=s.length();
+        for(int i=0;i<len;i++){
+            if(s[i]==']'){
+                string str;
+                while(st.top()!="["){
+                    str=st.top()+str;
+                    st.pop();
+                } 
+                st.pop();
+           string numStr;
+            while (!st.empty() && st.top().size() == 1 && isdigit(st.top()[0])) {
+                numStr += st.top();
+                st.pop();
             }
-            r += p;
+            reverse(numStr.begin(), numStr.end()); 
+            int num = stoi(numStr);
+                string res;
+                for(int k=0;k<num;k++)
+                 res+=str;
+                st.push(res);
+            } else {
+                st.push(string("") + s[i]);
+            }
         }
-        return {r, i};
-    }
-
-    pair<string, int> parseItem(string& s, int i) {
-        int n = s.length();
-        if (i >= n) {
-            return {"", n};
+        string res;
+        while(!st.empty()){
+            res=st.top()+res;
+            st.pop();
         }
-        if (s[i] == '[') {
-            return parse(s, i + 1); // Recursively parse inside brackets
-        }
-        if (s[i] == ']') {
-            return {string(1, '-1'), i + 1}; // Stop parsing when we reach ']'
-        }
-        if (!isdigit(s[i])) {
-            return {string(1, s[i]), i + 1}; // If it's a letter, add it to the result
-        }
-        int e = 0;
-        while (i < n && isdigit(s[i])) {
-            e = e * 10 + (s[i] - '0');
-            i++;
-        }
-        auto result = parseItem(s, i); // Parse the string inside the brackets
-        string r = result.first;
-        i = result.second;
-        string multiplied = "";
-        for (int j = 0; j < e; ++j) {
-            multiplied += r;
-        }
-        return {multiplied, i};
+        return res;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-
-        Solution ob;
-        cout << ob.decodedString(s) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
