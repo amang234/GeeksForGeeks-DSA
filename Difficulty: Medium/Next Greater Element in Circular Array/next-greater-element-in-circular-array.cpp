23c00827bd534ae27234d89d2c7e7e1f9d425c2a
Aman Grover <60424294+amang234@users.noreply.github.com>
2025-09-18 22:39:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.cpp
@@ -1,38 +1,25 @@
 class Solution {
   public:
-    vector<int> nextLargerElement(vector<int> &arr) {
-        stack<int> st;
+    vector<int> nextGreater(vector<int> &arr) {
         int n = arr.size();
-        vector<int> ans;
-        for(int i=0;i<n;i++)
-        {
-            arr.push_back(arr[i]);
+    vector<int> result(n, -1);
+    stack<int> st; // stores indices
+
+    // Traverse the array twice to simulate circular behavior
+    for (int i = 0; i < 2 * n; i++) {
+        int num = arr[i % n];
+
+        // Check if this num is the NGE for elements in stack
+        while (!st.empty() && arr[st.top()] < num) {
+            result[st.top()] = num;
+            st.pop();
         }
-        
-        for(int i=2*n-1;i>=0;i--)
-        {
-            while(!st.empty() && arr[i%n] >= st.top())
-            {
-                st.pop();
-            }
-            
-            if(i<n)
-            {
-                if(!st.empty())
-                {
-                    ans.push_back(st.top());
-                }
-                else
-                {
-                    ans.push_back(-1);
-                }
-            }
-            
-            st.push(arr[i%n]);
+
+        // Only push index during first pass
+        if (i < n) {
+            st.push(i);
         }
-        
-        reverse(ans.begin(), ans.end());
-        
-        return ans;
+    }
+    return result;
     }
 };
